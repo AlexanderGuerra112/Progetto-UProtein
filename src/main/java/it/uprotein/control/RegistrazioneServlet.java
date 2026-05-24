3ecc085cd59bb89ac5dc5661bac2c507bf1e997c
paolo <paolomariagentile@gmail.com>
2026-05-24 11:08:36 +0200

    registrazione servlet (mancata dal commit precedente)

--- src/main/java/it/uprotein/control/RegistrazioneServlet.java
+++ src/main/java/it/uprotein/control/RegistrazioneServlet.java
@@ -16,7 +16,7 @@ public class RegistrazioneServlet extends HttpServlet {
         
         String azione = request.getParameter("azione");
 
-        // Azione "mostra": Mostra il form vuoto per registrarsi
+        
         if (azione == null || azione.equalsIgnoreCase("mostra")) {
             request.getRequestDispatcher("/WEB-INF/views/common/registrazione.jsp").forward(request, response);
         }
@@ -24,8 +24,6 @@ public class RegistrazioneServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
-        // Qui dentro in un secondo momento metterai il codice per prendere i parametri 
-        // e fare il dao.doSave(utente) per salvarlo nel database!
         doGet(request, response);
     }
 }
